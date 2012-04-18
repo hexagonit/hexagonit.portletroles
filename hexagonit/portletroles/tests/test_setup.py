7d9d632fab790b916203a02c422e22b725fb7b36
Taito Horiuchi <taito.horiuchi@hexagonit.fi>
2012-04-18 09:56:48 +0300

    Test for uninstall added.

--- hexagonit/portletroles/tests/test_setup.py
+++ hexagonit/portletroles/tests/test_setup.py
@@ -8,16 +8,30 @@ class TestCase(IntegrationTestCase):
     def setUp(self):
         self.portal = self.layer['portal']
 
-    def test_is_hexagonit_portletroles_installed(self):
+    def test_package_installed(self):
         installer = getToolByName(self.portal, 'portal_quickinstaller')
         self.failUnless(installer.isProductInstalled('hexagonit.portletroles'))
+
+    def test_browserlayer(self):
+        from hexagonit.portletroles.browser.interfaces import IHexagonitPortletrolesLayer
+        from plone.browserlayer import utils
+        self.failUnless(IHexagonitPortletrolesLayer in utils.registered_layers())
+
+    def test_metadata__version(self):
+        setup = getToolByName(self.portal, 'portal_setup')
+        self.assertEqual(
+            setup.getVersionForProfile('profile-hexagonit.portletroles:default'),
+            u'1'
+        )    
 
     def test_uninstall__hexagonit_portletroles_not_installed(self):
         installer = getToolByName(self.portal, 'portal_quickinstaller')
         installer.uninstallProducts(['hexagonit.portletroles'])
         self.failIf(installer.isProductInstalled('hexagonit.portletroles'))
 
-    def test_browserlayer(self):
+    def test_uninstall__browserlayer(self):
+        installer = getToolByName(self.portal, 'portal_quickinstaller')
+        installer.uninstallProducts(['hexagonit.portletroles'])
         from hexagonit.portletroles.browser.interfaces import IHexagonitPortletrolesLayer
         from plone.browserlayer import utils
-        self.failUnless(IHexagonitPortletrolesLayer in utils.registered_layers())
+        self.failIf(IHexagonitPortletrolesLayer in utils.registered_layers())
